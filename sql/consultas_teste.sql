bd6ad2be136076d4f58a60fcd992fabe62f0520c
Igor Dias Aguiar <dias2005aguiar@gmail.com>
2025-06-28 16:50:57 -0300

    Ajuste nas consultas teste

--- sql/consultas_teste.sql
+++ sql/consultas_teste.sql
@@ -11,19 +11,16 @@ ORDER BY v.data_venda;
 
 
 -- Consulta 2: Quais são os gêneros de cada jogo?
--- Esta consulta testa a nossa tabela de junção "muitos-para-muitos" (jogo_genero).
 SELECT
     j.titulo,
     g.nome_genero
 FROM jogos AS j
-JOIN jogo_genero AS jg ON j.jogo_id = jg.jogo_id -- Conecta o jogo à tabela de junção
-JOIN generos AS g ON jg.genero_id = g.genero_id   -- Conecta a junção ao nome do gênero
+JOIN jogo_genero AS jg ON j.jogo_id = jg.jogo_id 
+JOIN generos AS g ON jg.genero_id = g.genero_id   
 ORDER BY j.titulo, g.nome_genero;
 
 
 -- Consulta 3: Quais foram as avaliações dadas, por quem e para qual jogo?
--- Esta consulta valida a tabela 'avaliacoes' que você sugeriu, mostrando
--- o nome do usuário, o título do jogo e a nota que ele deu.
 SELECT
     j.titulo,
     c.nome_usuario AS avaliador,
